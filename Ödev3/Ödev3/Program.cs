0aea68cbc07abb3db66d8ac74bd715e4a87dbdec
ftm <99824014+ftmakdeniz@users.noreply.github.com>
2023-05-31 21:24:27 +0300

    Add files via upload

---
+++ Ödev3/Ödev3/Program.cs
@@ -0,0 +1,68 @@
+﻿//for (int i = 1; i <= 7; i++)
+//{
+//    for (int j = 1; j <= 7 - i; j++)
+//    {
+//Console.Write(" ");
+//    }
+//    for (int k = 1; k <= 2 * i - 1; k++)
+//{
+//Console.Write("*");
+//}
+//Console.WriteLine();
+//}
+
+//for (int i = 13; i >= 1; i--)
+//{
+//for (int j = 1; j <= i; j++)
+//{
+//Console.Write("*");
+//}
+//Console.WriteLine();
+
+//}
+
+//string name = "FATMA";
+
+//for (int i = 0; i < name.Length; i++)
+//{
+//Console.WriteLine(name[i]);
+//}
+
+
+//for (int i = 1; i <= 13; i++)
+//{
+//for (int j = 1; j <= i; j++)
+//{
+//Console.Write("*");
+//}
+//Console.WriteLine();
+//}
+
+
+//Console.Write("Kaç adet ürün gireceksiniz? ");
+//int urunSayisi = int.Parse(Console.ReadLine());
+
+//string[] urunler = new string[urunSayisi];
+////decimal[] fiyatlar = new decimal[urunSayisi];
+
+////for (int i = 0; i < urunSayisi; i++)
+////{
+////    Console.Write($"Ürün {i + 1} adı: ");
+//    urunler[i] = Console.ReadLine();
+
+//    Console.Write($"Ürün {i + 1} fiyatı: ");
+//    fiyatlar[i] = decimal.Parse(Console.ReadLine());
+//}
+
+//Console.WriteLine("Ürünler ve Fiyatları:");
+//for (int i = 0; i < urunSayisi; i++)
+//{
+//    Console.WriteLine($"{urunler[i]} - {fiyatlar[i]}");
+//}
+
+
+
+
+
+
+
